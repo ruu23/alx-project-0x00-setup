b257e4d928623d7445f23a3fc95b8666d0ca2a0e
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-12 14:17:43 +0200

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -16,6 +16,7 @@ const Landing: React.FC =  () => {
           <Button title="Small - Rounded SM" styles="text-sm rounded-sm px-3 py-1" />
           <Button title="Small - Rounded MD" styles="text-sm rounded-md px-3 py-1" />
           <Button title="Small - Rounded Full" styles="text-sm rounded-lg px-3 py-1" />
+          <Button title="Small - Rounded Full" styles="text-sm rounded-full px-3 py-1" />
         </div>
 
         {/* Medium Buttons */}
@@ -23,6 +24,7 @@ const Landing: React.FC =  () => {
           <Button title="Medium - Rounded SM" styles="text-base rounded-sm px-4 py-2" />
           <Button title="Medium - Rounded MD" styles="text-base rounded-md px-4 py-2" />
           <Button title="Medium - Rounded Full" styles="text-base rounded-lg px-4 py-2" />
+          <Button title="Medium - Rounded Full" styles="text-base rounded-full px-4 py-2" />
         </div>
 
         {/* Large Buttons */}
@@ -30,6 +32,7 @@ const Landing: React.FC =  () => {
           <Button title="Large - Rounded SM" styles="text-lg rounded-sm px-6 py-3" />
           <Button title="Large - Rounded MD" styles="text-lg rounded-md px-6 py-3" />
           <Button title="Large - Rounded Full" styles="text-lg rounded-lg px-6 py-3" />
+          <Button title="Large - Rounded Full" styles="text-lg rounded-full px-6 py-3" />
       </div>
     </div>
     </div>
